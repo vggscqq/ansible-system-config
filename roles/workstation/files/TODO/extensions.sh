2766cc18ac73de04cb60ca0f16afc4f063c8a81d
vgscq <vggscqq@gmail.com>
2024-07-08 22:50:40 +0200

    321

---
+++ roles/workstation/files/TODO/extensions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gext install 307
+gext install 4338
+gext install 517
+gext install 4839
+gext install 2932
+gext install 5410
+gext install 1319
+gext install 600
+gext install 4727
